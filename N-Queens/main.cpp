b79a674b524c272fd83512580f2662ec3c00548c
Jonson Jin <jonsonjin@jonsons-mbp.fios-router.home>
2018-07-04 22:41:45 -0400

    Initial Commit

---
+++ N-Queens/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  N-Queens
+//
+//  Created by Jonson Jin on 7/4/18.
+//  Copyright © 2018 Jonson Jin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
